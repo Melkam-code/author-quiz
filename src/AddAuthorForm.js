7a45fc6563218d1535e15f643abfff9e7bb9c161
melkam <melkammekonnen899@gmail.com>
2021-07-29 18:11:34 +0300

    Making auhor form a component

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './AddAuthorForm.css';
+
+class AuthorForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            imageUrl: ''
+        }
+        this.onFieldChange = this.onFieldChange.bind(this);
+    }
+
+    onFieldChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    render(){
+        return(
+            <form>
+            <div className="AddAuthorForm_input">
+                <lable htmlFor="name">Name</lable>
+                <input type="text" name="name" value={this.state.name} onChange={this.onFieldChange} />
+            </div>
+            <div className="AddAuthorForm_input">
+                <lable htmlFor="imageUrl">Image URL</lable>
+                <input type="text" name="imageUrl" value={this.state.imageUrl} onChange={this.onFieldChange} />
+            </div>
+        </form>
+        )
+    }
+}
 
 function AddAuthorForm({ match }){
     return(
       <div className="AddAuthorForm">
         <h1>Add Author</h1>
-        <form>
-            <div className="AddAuthorForm_input">
-                <lable htmlFor="name">Name</lable>
-                <input type="text" name="name" />
-            </div>
-        </form>
+        <AuthorForm />
       </div>
     )
   }
